d92b33c4c9ab56e536d40e3a319c91a4a14bbe66
Glitch (glitch-blank-node) <none>
2022-05-08 14:37:15 +0000

    💓☄️ Checkpoint ./server.js:44601265/83 ./src/pages/index.hbs:44601265/51

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ function sleep (time) {
 //removeme
 
 // Our main GET home page route, pulls from src/pages/index.hbs
-fastify.get("/susai", function (request, reply) {
+fastify.get("/susballer", function (request, reply) {
   
   var imgURL = "";
 
@@ -37,11 +37,12 @@ fastify.get("/susai", function (request, reply) {
   
   var sleepTime = 1;
   
-  if (!fs.existsSync("~/public/cowpoland" + request.ip + imgEXT).exists()) {
+  if (!fs.existsSync("/app/public/cowpoland" + request.ip + imgEXT)) {
   
     //require('child_process').spawn('bash', ['-c', 'ffmpeg -i polish-cow.jpg -vf "drawtext=fontfile=comic-sans.ttf:text=\'' + request.ip + '\':fontcolor=white:fontsize=50:box=1:boxcolor=black@0.5:boxborderw=5:x=50:y=50" -codec:a copy public/cowpoland' + request.ip + '.jpg'])
     /* gif version */ require('child_process').spawn('bash', ['-c', 'ffmpeg -i polish-cow.gif -vf "drawtext=fontfile=comic-sans.ttf:text=\'' + request.ip + '\':fontcolor=white:fontsize=50:box=1:boxcolor=black@0.5:boxborderw=5:x=50:y=50" -codec:a copy public/cowpoland' + request.ip + '.gif'])
     sleepTime = 3000;
+    console.log("pain");
   }
   sleep(sleepTime).then(() => {
     imgURL = site_url + "/gifs/cowpoland" + request.ip + imgEXT;
@@ -49,7 +50,8 @@ fastify.get("/susai", function (request, reply) {
     
     let params = {
       link_to_img: imgURL,
-      site_url: site_url
+      site_url: site_url,
+      ip: request.ip
     };
     // request.query.paramName <-- a querystring example
     reply.view("/src/pages/index.hbs", params);
